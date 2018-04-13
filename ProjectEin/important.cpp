053c28d52c17a39b4ec68a4742d6b5f62edb6312
anyaliang <anyaliangww@gmail.com>
2018-04-14 05:47:53 +0800

    Update

---
+++ ProjectEin/important.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#define SymbolTable sym_t
+
+using namespace std;
+    
+class SymbolTable{
+public:
+    vector <string> V;
+    map<string, int> M;
+    int size;
+};
+
+sym_t* st;
+
+ //funcs
+sym_t* Create(void){
+    st = new SymbolTable();
+    st->size = 0;
+}
+int Insert(string s){
+    (st->M).insert(pair<string,int>(s, st->size ));        
+    (st->V).push_back(s);
+    return (st->size)++;
+}
+
+int LookUp(string s){
+    if ((st->M).find(s)==(st->M).end()){
+        return -1;
+     } //s not in map M's keys
+    else {
+        return (st->M)[s];
+    }
+}
+
+int Dump(void){
+    for(std::vector<string>::iterator it=(st->V).begin(); it!=(st->V).end(); it++){
+        cout << *it << endl;
+    }
+    return (st->V).size();
+}
+
+int main(void){
+    Create();
+
+    cout << Insert("abc") << endl;
+    cout << Insert("def") << endl;
+    cout << Insert("ccc") << endl;
+    cout << Insert("asdfadsf") << endl;
+    cout << LookUp("ccc") << endl;
+    cout << LookUp("def") << endl;
+    
+    cout << Dump();
+    return 0;
+}
